d57ce38102a9b753f1acf483c900a26b77300e08
Vinicius Teixeira <99350364+viniciushammett@users.noreply.github.com>
2023-05-17 22:30:56 -0300

    Terraform-Docker-Beanstalk-AWS

---
+++ Terraform-Docker-Beanstalk-AWS/env/prod/main.tf
@@ -0,0 +1,9 @@
+module "Producao" {
+  source = "../../infra"
+
+  nome = "producao"
+  descricao = "aplicacao-de-producao"
+  max = 5
+  maquina = "t2.micro"
+  ambiente = "ambiente-de-producacao"
+}
